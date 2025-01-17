5fda63fcab42722324985e501e7790977c74b613
dima <ds-drozdov@yandex.ru>
2025-01-17 22:27:50 +0300

    run on windows arm64

--- LandParserGenerator/Land.Markup/Binding/FuzzyHashing.cs
+++ LandParserGenerator/Land.Markup/Binding/FuzzyHashing.cs
@@ -4,44 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using TrendMicro.Tlsh;
+using System.Net.Http.Headers;
 
 namespace Land.Markup.Binding
 {
-	public static class FuzzyHashing
-	{
-		public const int MIN_TEXT_LENGTH = 25;
-		public const int TLSH_HASH_LENGTH = 138;
+    // TODO: нерабочий метод
+    public static class FuzzyHashing
+    {
+        public const int MIN_TEXT_LENGTH = 25;
+        public const int TLSH_HASH_LENGTH = 138;
 
-		public static byte[] GetFuzzyHash(string text)
-		{
-			using (var tlshObject = new TLSH.HashObject())
-			{
-				var textBytes = Encoding.Unicode.GetBytes(text);
-				tlshObject.final(textBytes, (uint)textBytes.Length, 1);
 
-				return tlshObject.getHash()
-					.Take(TLSH_HASH_LENGTH).ToArray();
-			}
-		}
+        public static byte[] GetFuzzyHash(string text)
+        {
+            if (Encoding.UTF8.GetByteCount(text)<512)
+            {
+                //throw new Exception($"{Encoding.UTF8.GetByteCount(text)}");
+                return new byte[] { };
+            }
+            var t = new Tlsh();
+            t.Update(Encoding.Unicode.GetBytes(text));
+            var res = t.GetHash();
+            return res.ToByteArray().ToArray();
+        }
 
-		public static double CompareTexts(string txt1, string txt2)
-		{
-			var hash1 = GetFuzzyHash(txt1);
-			var hash2 = GetFuzzyHash(txt2);
+        public static double CompareTexts(string txt1, string txt2)
+        {
+            var hash1 = GetFuzzyHash(txt1);
+            var hash2 = GetFuzzyHash(txt2);
 
-			return CompareHashes(hash1, hash2);
-		}
+            return CompareHashes(hash1, hash2);
+        }
 
-		public static double CompareHashes(byte[] hash1, byte[] hash2)
-		{
-			using (var tlsh1 = new TLSH.HashObject())
-			using (var tlsh2 = new TLSH.HashObject())
-			{
-				tlsh1.fromTlshStr(hash1);
-				tlsh2.fromTlshStr(hash2);
+        public static double CompareHashes(byte[] hash1, byte[] hash2)
+        {
+            var d = ComputeHashDistance(hash1, hash2);
+            return Math.Max(0, (300 - d) / 300.0);
+        }
 
-				return Math.Max(0, (300 - tlsh1.totalDiff(tlsh2)) / 300.0);
-			}
-		}
-	}
+
+        static int ComputeHashDistance(byte[] hash1, byte[] hash2)
+        {
+            if (hash1.Length != hash2.Length)
+            {
+                return Math.Max(hash1.Length, hash2.Length);
+            }
+
+            int distance = 0;
+            for (int i = 0; i < hash1.Length; i++)
+            {
+                if (hash1[i] != hash2[i])
+                {
+                    distance++;
+                }
+            }
+            return distance;
+        }
+
+
+    }
 }
---
+++ LandParserGenerator/Land.Markup/Binding/FuzzyHashingTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using Land.Markup.Binding;
+using System.Runtime.Remoting.Messaging;
+
+namespace Land.Markup.Tests
+{
+    [TestFixture]
+    public class FuzzyHashingTests
+    {
+       /* [Test]
+        public void CompareTexts_IdenticalVariedTexts_Returns1()
+        {
+            // Arrange
+            string text1 = "The quick brown fox jumps over the lazy dog 1234567890"
+                         + "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                         + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+                         + "!@#$%^&*()_+<>?/|{}[]`~";
+            string text2 = text1;
+
+            // Act
+            double similarity = FuzzyHashing.CompareTexts(text1, text2);
+
+            // Assert
+            Assert.AreEqual(1.0, similarity, 0.0001);
+        }*/
+
+        [Test]
+        public void CompareTexts_CompletelyDifferentVariedTexts_Returns0()
+        {
+            // Arrange
+            string text1 = "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife. However little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families, that he is considered the rightful property of some one or other of their daughters. 'My dear Mr. Bennet,' said his lady to him one day, 'have you heard that Netherfield Park is let at last?'";
+
+            string text2 = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way—in short, the period was so far like the present period.";
+
+            // Act
+            double similarity = FuzzyHashing.CompareTexts(text1, text2);
+
+            // Assert
+            Assert.AreEqual(0.0, similarity, 0.0001);
+        }
+
+        [Test]
+        public void CompareTexts_SlightlyDifferentVariedTexts_ReturnsHighSimilarity()
+        {
+            // Arrange
+            string text1 = "The quick brown fox jumps over the lazy dog 1234567890"
+                         + "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                         + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+                         + "!@#$%^&*()_+<>?/|{}[]`~";
+
+            string text2 = text1 + " Just a bit of extra data at the end.";
+
+            // Act
+            double similarity = FuzzyHashing.CompareTexts(text1, text2);
+
+            // Assert
+            Assert.GreaterOrEqual(similarity, 0.8);
+        }
+    }
+}
